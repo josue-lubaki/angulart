85dbed6fb058016e20fb924ff270805040d7aa1e
Josue Lubaki <josuelubaki@gmail.com>
2022-02-06 08:27:15 -0500

    improvement reservation component UI

--- src/app/pages/my-reservation/my-reservation.component.ts
+++ src/app/pages/my-reservation/my-reservation.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {DataImService} from "../../services/data-im.service";
+import { Haircut } from 'src/app/models/Haircut';
+import { DataImService } from '../../services/data-im.service';
 
 @Component({
   selector: 'app-my-reservation',
   templateUrl: './my-reservation.component.html',
-  styleUrls: ['./my-reservation.component.scss']
+  styleUrls: ['./my-reservation.component.scss'],
 })
 export class MyReservationComponent implements OnInit {
+  haircuts: Haircut[] = [];
 
-
-  constructor(public data:DataImService) {
-
+  constructor(public data: DataImService) {
+    this.haircuts = this.data.getHaircuts();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
