6feeb0fe568732c806a906b0449e3c3aaccf66cb
Josue Lubaki <josuelubaki@gmail.com>
2022-02-06 05:42:52 -0500

    add localStorage service for login user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentComponent } from './content/content.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 
 // Path of signup component
-import { ObjectifComponent } from './signup/pages/objectif/objectif.component';
-import { ProfilComponent } from './signup/pages/profil/profil.component';
-import { AdresseComponent } from './signup/pages/adresse/adresse.component';
-
 // Primeng - Module
 import { StepsModule } from 'primeng/steps';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
-import { SignUpModule } from './signup/signup.module';
 import { CommonModule } from '@angular/common';
-import { HomePageModule } from './home-page/home-page.module';
-
+import { AdresseComponent } from './pages/signup/pages/adresse/adresse.component';
+import { ObjectifComponent } from './pages/signup/pages/objectif/objectif.component';
+import { ProfilComponent } from './pages/signup/pages/profil/profil.component';
+import { ContentComponent } from './pages/content/content.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomePageModule } from './pages/home-page/home-page.module';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SignUpModule } from './pages/signup/signup.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -55,7 +53,7 @@ const primengModules = [StepsModule, ButtonModule, CardModule, ToolbarModule];
     ReactiveFormsModule,
     CommonModule,
     SignUpModule,
-    HomePageModule
+    HomePageModule,
   ],
   exports: [RouterModule, ...primengModules],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import {MyReservationComponent} from "./my-reservation/my-reservation.component";
-import {DetailReservationComponent} from "./detail-reservation/detail-reservation.component";
-import { HomePageComponent } from './home-page/home-page.component';
+import { DetailReservationComponent } from './pages/detail-reservation/detail-reservation.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { MyReservationComponent } from './pages/my-reservation/my-reservation.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavigationComponent, MyReservationComponent, DetailReservationComponent],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    MyReservationComponent,
+    DetailReservationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
