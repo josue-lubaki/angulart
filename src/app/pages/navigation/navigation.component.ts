52e4cf539c8b8dd164a6426dce8945f855a40236
Josue Lubaki <josuelubaki@gmail.com>
2022-02-07 06:45:40 -0500

    add fonctionality Hamburger #OK

--- src/app/pages/navigation/navigation.component.ts
+++ src/app/pages/navigation/navigation.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthUserService } from 'src/app/services/auth-user.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { User } from '../signup/models/User';
+
+/* Fonction qui permet à l'icône "Hamburger" d'afficher la barre de menu */
+declare function showMenuBar(): void;
+declare function linkAction(): void;
 
 @Component({
   selector: 'app-navigation',
@@ -13,15 +19,17 @@ export class NavigationComponent implements OnInit, OnDestroy {
   avatar?: string;
   user?: User;
 
-  constructor(private authUserService: AuthUserService) {
+  constructor(
+    private authUserService: AuthUserService,
+    private router: Router,
+    private localStorage: LocalStorageService
+  ) {
     // enregistrer variable user comme observer de userConnected$ du service authUserService
     // Dès qu'il y a une modification sur userConnected$, il sera notifier
     this.authUserService.userConnected$.subscribe((user: User) => {
       this.user = user;
       this.avatar = user?.image;
     });
-
-    console.log('The User is : ', this.user);
   }
 
   ngOnInit(): void {
@@ -31,7 +39,6 @@ export class NavigationComponent implements OnInit, OnDestroy {
       this.user = this.authUserService.getUserConnected();
       this.avatar = this.user.image;
     }
-    console.log('The User is : ', this.user);
   }
 
   /**
@@ -45,9 +52,26 @@ export class NavigationComponent implements OnInit, OnDestroy {
     //this.authUserService.userConnected$.subscribe().unsubscribe();
   }
 
+  /**
+   * Cette Methode appelle la fonction showMenuBar() qui est declarée dans le fichier header.js
+   * Elle permet d'afficher la barre de menu
+   */
   openMenuBar() {
-
+    showMenuBar();
   }
 
-  
+  /**
+   * Cette Methode appelle la fonction linkAction() qui est declarée dans le fichier header.js
+   * Elle permet de fermer la barre de menu
+   */
+  closeMenuBar() {
+    linkAction();
+  }
+
+  closeMenuBarAndDeconnexion() {
+    this.closeMenuBar();
+    this.localStorage.removeToken();
+    this.localStorage.removeUserCurrent();
+    this.router.navigate(['/']);
+  }
 }
